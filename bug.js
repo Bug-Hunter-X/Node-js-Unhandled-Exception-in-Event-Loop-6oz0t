e9464f432dca5eb9643e163df9391545530faf36
BugHunter-X <askzenai@gmail.com>
2025-01-05 13:06:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Handle requests here
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+//This is a simple HTTP server. There is nothing wrong with this code, it's just a simple example to show that this is where you could put your bug code.
